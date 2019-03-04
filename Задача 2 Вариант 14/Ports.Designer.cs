658ef4da753573c40cf9a8e07677e8e4ee54f6f0
Ilya Kochankov <fakafafakafa@mail.ru>
2019-03-04 11:46:15 +0300

    begin

--- Задача 2 Вариант 14/Ports.Designer.cs
+++ Задача 2 Вариант 14/Ports.Designer.cs
@@ -1,13 +1,13 @@
 ﻿namespace Задача_2_Вариант_14
 {
-    partial class Form1
+    partial class Ports
     {
-        /// <summary>
+        /// <summary> 
         /// Обязательная переменная конструктора.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
+        /// <summary> 
         /// Освободить все используемые ресурсы.
         /// </summary>
         /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
@@ -20,40 +20,24 @@
             base.Dispose(disposing);
         }
 
-        #region Код, автоматически созданный конструктором форм Windows
+        #region Код, автоматически созданный конструктором компонентов
 
-        /// <summary>
+        /// <summary> 
         /// Требуемый метод для поддержки конструктора — не изменяйте 
         /// содержимое этого метода с помощью редактора кода.
         /// </summary>
         private void InitializeComponent()
         {
-            this.ultraSonick1 = new Задача_2_Вариант_14.UltraSonick();
             this.SuspendLayout();
             // 
-            // ultraSonick1
+            // Ports
             // 
-            this.ultraSonick1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ultraSonick1.Location = new System.Drawing.Point(75, 112);
-            this.ultraSonick1.Name = "ultraSonick1";
-            this.ultraSonick1.Size = new System.Drawing.Size(332, 234);
-            this.ultraSonick1.TabIndex = 0;
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(489, 450);
-            this.Controls.Add(this.ultraSonick1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "Ports";
+            this.Size = new System.Drawing.Size(150, 337);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private UltraSonick ultraSonick1;
     }
 }
-
---
+++ Задача 2 Вариант 14/Ports.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Задача_2_Вариант_14
+{
+    public partial class Ports : ComboBox
+    {
+        public Ports()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
